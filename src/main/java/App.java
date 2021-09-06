65eda02fd82c74752d1b1af51e76cbde658a0002
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-06 16:05:16 +0300

    Added afew new files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
+import static spark.Spark.staticFileLocation;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+    }
 }
